788c11a94aee0ef35e88fe033a7bbc9aeb2d4c37
Mayank Srivastava <mayankdiatm@gmail.com>
2016-08-03 20:59:36 +0530

    Take A Break Code Comple -- Udacity

--- takeabreak.py
+++ takeabreak.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+import time
+import webbrowser
+
+a = time.strftime("%I:%M:%S")
+print(time.strftime(a))
+break_count=4
+count =0
+while(count<break_count):
+    print("====== Current Time When Program Started is ======"+ time.ctime())
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/")
+    count = count +1
+
+print "Good Bye!"
+
 
 
 
