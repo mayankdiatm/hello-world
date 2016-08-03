8180666ce7859cbda99a89b14db2c9a6522c9dec
Mayank Srivastava <mayankdiatm@gmail.com>
2016-08-03 22:51:32 +0530

    temp

--- secretMessage.py
+++ secretMessage.py
@@ -2,8 +2,16 @@
 import os 
 
 def rename_files():
+    # (1) Scans a directory and prints the contents it has
     file_list = os.listdir("/home/maverick/Downloads/prank")
     print(file_list)
+    saved_path = os.getcwd()
+    print("Current Working Directory is " + saved_path)
+    os.chdir("/home/maverick/Downloads/prank") 
+    # (2) Read the file name and remove numbers it contains
+    for file_name in file_list:
+        os.rename(file_name,file_name.translate(None, "0123456789"))
+       
 
 
 rename_files()
