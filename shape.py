34b518771fe9c733c0139e411b20de8e6a63cdf6
Mayank Srivastava <mayankdiatm@gmail.com>
2016-08-04 20:39:08 +0530

    new

--- shape.py
+++ shape.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(200)
+        some_turtle.right(90)
+
+def draw_art():
     window =turtle.Screen()
-    window.bgcolor("red")
-    
+    window.bgcolor("black")
     brad = turtle.Turtle()
-    brad.forward(120)
-    brad.right(90)
-    brad.forward(120)
-    brad.right(90)
-    brad.forward(120)
-    brad.right(90)
-    brad.forward(120)
-    brad.right(90)
-    brad.forward(120)
-    brad.right(90)
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+#    draw_square(brad)
+    for i in range(1,74):
+        draw_square(brad)
+        brad.right(5)
+    
+   # angie = turtle.Turtle()
+   # angie.shape("arrow")
+   # angie.color("blue")
+   # angie.circle(200)
+  
+
+    window.exitonclick()
 
 
 
 
 
-
-draw_square()
+draw_art()
