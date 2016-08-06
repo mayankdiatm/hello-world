34dd55bd5a55b902e0dd1050aa2751e359dd5667
Mayank Srivastava <mayankdiatm@gmail.com>
2016-08-06 19:33:40 +0530

    Movies

---
+++ movies/entertainment_center.py
@@ -0,0 +1,14 @@
+import media
+
+toy_story = media.Movies("Toy Story",
+                         "Woody, Bo Peep, Rex, Slinky, Mr. Potato Head, Hamm, Lenny, Slinky, Mr. Spell, and a few others are his toys. Then, as a rivalry begins between Woody and new toy Buzz. A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room",
+                         "https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg",
+                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+#print(toy_story.storyline)
+toy_story.show_trailer()
+avatar = media.Movies("Avatar",
+                      "When his brother is killed in a robbery, paraplegic Marine Jake Sully decides to take his place in a mission on the distant world of Pandora.",
+                      "https://en.wikipedia.org/wiki/Avatar_(2009_film)#/media/File:Avatar-Teaser-Poster.jpg",
+                      "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+#print (avatar.storyline)
+avatar.show_trailer()
